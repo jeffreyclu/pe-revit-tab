3f742b5d01d58b48b64235e2ce137b00bcb66df5
Jeffrey Lu <j.lu@perkinseastman.com>
2020-06-29 15:06:02 -0400

    this commit added links to sharepoint lists cp_revit_models and cp_project_permissions

--- PERevitTabCodebase/Data/SharepointConstants.cs
+++ PERevitTabCodebase/Data/SharepointConstants.cs
@@ -26,8 +26,7 @@ namespace PERevitTab.Data
             public static string siteUrl = @"https://perkinseastman.sharepoint.com/sites/Grove";
             public static string spReadList = @"cp_published_volumes";
             public static string spWriteList = @"cp_revit_volumes";
-            public static string userListName = @"translatedUser";
-            public static string projectListName = @"projects";
+            public static string spModelsList = @"cp_revit_models";
             public static string projectPermissionsListName = @"cp_project_permissions";
             public static string allItems = "All Items";
         }
