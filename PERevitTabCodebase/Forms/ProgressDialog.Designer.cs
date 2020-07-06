21804b797594e5468fb62c94fabebc48bdbcb1b3
Jeffrey Lu <j.lu@perkinseastman.com>
2020-07-06 09:51:00 -0400

    this commit added a reusable progress dialog box and increment public method

---
+++ PERevitTabCodebase/Forms/ProgressDialog.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PERevitTab.Forms
+{
+    partial class ProgressDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.taskLabel = new System.Windows.Forms.Label();
+            this.progressLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 59);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(610, 40);
+            this.progressBar.TabIndex = 0;
+            // 
+            // taskLabel
+            // 
+            this.taskLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taskLabel.Location = new System.Drawing.Point(12, 22);
+            this.taskLabel.Name = "taskLabel";
+            this.taskLabel.Size = new System.Drawing.Size(490, 25);
+            this.taskLabel.TabIndex = 1;
+            this.taskLabel.Text = "Current Task:";
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.progressLabel.Location = new System.Drawing.Point(555, 22);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(67, 25);
+            this.progressLabel.TabIndex = 2;
+            this.progressLabel.Text = "0%";
+            this.progressLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // ProgressDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 111);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.taskLabel);
+            this.Controls.Add(this.progressBar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProgressDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "ProgressDialog";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.ProgressDialog_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label taskLabel;
+        private System.Windows.Forms.Label progressLabel;
+    }
+}
---
+++ PERevitTabCodebase/Forms/ProgressDialog.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PERevitTab.Forms
+{
+    public partial class ProgressDialog : Form
+    {
+        private int _increment { get; set; }
+        public ProgressDialog(string name, int totalTasks)
+        {
+            Text = name;
+            _increment = (int)Math.Floor(100 / (double)totalTasks);
+            InitializeComponent();
+        }
+
+        private void ProgressDialog_Load(object sender, EventArgs e)
+        {
+            // set progressbar initial values
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            progressBar.Value = 0;
+
+            // set label initial values
+            progressLabel.Text = $"{progressBar.Value}%";
+            taskLabel.Text = "Current Task:";
+        }
+
+        public void Increment()
+        {
+            // increment the progress bar value
+            progressBar.Value += _increment;
+
+            // update the progress percentage 
+            progressLabel.Text = $"{progressBar.Value}%";
+            progressLabel.Refresh();
+        }
+
+        public void StartTask(string taskName)
+        {
+            // check if taskName is greater than 35 characters and slice it if it is
+            string taskLabelText = taskName.Length > 35 ? $"{taskName.Substring(0, 31)} ..." : taskName;
+
+            // update tasklabel
+            taskLabel.Text = $"Current Task: {taskLabelText}";
+            taskLabel.Refresh();
+        }
+    }
+}
